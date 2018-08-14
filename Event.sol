8bc53769e3c499895c290a1082f7a7c1468f35d0
yangbinnnn <yangbinnnn@gmail.com>
2018-08-14 14:49:43 +0800

    update event

--- Event.sol
+++ Event.sol
@@ -8,9 +8,27 @@ contract EventContract {
         uint256 value
     );
     
-    function dopoist(uint256 id) public payable {
+    function deposit(uint256 id) public payable {
         emit Deposit(msg.sender, id, msg.value);
     }
+    
 }
 
 
+/**
+ * 
+ * contract = web3.eth.contract(abi).at('0x49590975567cadf7ddb5e524175055237c5faae4')
+ * event = contract.Deposit([], [{fromBlock: 0, toBlock: "latest"}])
+ * 
+ * // get all event
+ * event.get(function(error, data) {
+ *   console.log(data) 
+ * })
+ * 
+ * // watch for event
+ * event.watch(function(error, data) {
+ *   console.log(data)
+ * })
+ *
+ * 
+ **/ 
---
+++ diving/c1.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.4.11;
+// https://blog.qtum.org/diving-into-the-ethereum-vm-6e8d5d2f3c30
+contract C {
+    uint256 a;
+    function C() {
+      a = 1;
+    }
+}
